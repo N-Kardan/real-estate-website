ecfb13bad73728fbb085d0a9a8894f3fb15a305d
Nazar Kardan <nazar.kardan@gmail.com>
2026-03-02 02:26:13 +0300

    Create footer section

--- components/FooterMain.tsx
+++ components/FooterMain.tsx
@@ -2,7 +2,17 @@ import Styles from './FooterMain.module.css';
 
 const FooterMain = () => {
   return (
-    <div>FooterMain</div>
+    <>
+    <section className={Styles.footerMain}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            <h1>Footer Main</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
